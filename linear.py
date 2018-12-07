05cf3f0b76fac97b179fadc20ed4136edeae08ae
vhorbach <vh@morbax.com>
2018-12-07 16:48:59 +0200

    plot added

--- linear.py
+++ linear.py
@@ -1,71 +1,66 @@
 import numpy as np
-import matplotlib as mtp
+import matplotlib.pyplot as plt
 import math
+import argparse
 
 
 def equasion(theta0, theta1, x):
     return theta0 + (theta1 * x)
 
 
+def standard_error(x, y, theta0, theta1):
+    res = 0
+    for i in range(len(x)):
+        res += (equasion(theta0, theta1, x[i]) - y[i]) ** 2
+    return math.sqrt(res / len(x) - 2)
+
+
 def cost_0(x, y, theta0, theta1, len):
     res = 0.
-    for n in range(len-1):
+    for n in range(len):
         res += equasion(theta0, theta1, x[n]) - y[n]
     return res / len
 
 
 def cost_1(x, y, theta0, theta1, len):
     res = 0.
-    for n in range(len-1):
+    for n in range(len):
         res += (equasion(theta0, theta1, x[n]) - y[n]) * x[n]
     return res / len
 
 
 def normalize_x(x: list):
     x_n = list()
-    print(max(x), min(x))
     for z in range(len(x)):
         x_n.append((x[z] - min(x))/ (max(x) - min(x)))
     return x_n
 
+
 def normalize_one(x, km):
     return (x - min(km))/ (max(km) - min(km))
 
-def estimate(t0, t1, km, price, l_rate):
-    t0_tmp = 0
-    t1_tmp = 0
-    for i in range(len(km)):
-        t0_tmp += equasion(t0, t1, km[i]) - price[i]
-        t1_tmp += ((equasion(t0, t1, km[i])) - price[i]) * km[i]
-    t0 = t0 - (l_rate * t0_tmp) / len(km)
-    t1 = t1 - (l_rate * t1_tmp) / len(km)
-    return t0, t1
 
 if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument()
     km = list()
     price = list()
-    with open("data.csv") as f:
+    with open("data.csv", "r") as f:
+        next(f)
         for line in f:
             k, p = line.rstrip().split(',')
             p.rstrip()
-            if k == 'km':
-                continue
-            elif p == 'price':
-                continue
             km.append(int(k))
             price.append(int(p))
-
     alpha = 0.1
     theta_0 = 0
     theta_1 = 0
     km_n = normalize_x(km)
-    # price_n = normalize_x(price)
-    price_n = price
-    print(km_n, price_n)
     t_tmp_0 = 0
     t_tmp_1 = 0
     while True:
-        t_tmp_0, t_tmp_1 = estimate(theta_0, theta_1, km_n, price_n, alpha)
+        t_tmp_0 = t_tmp_0 - (alpha * cost_0(km_n, price, t_tmp_0, t_tmp_1, len(km))) / len(km)
+        t_tmp_1 = t_tmp_1 - (alpha * cost_1(km_n, price, t_tmp_0, t_tmp_1, len(km))) / len(km)
         if t_tmp_0 == theta_0 and t_tmp_1 == theta_1:
             break
         else:
@@ -74,3 +69,9 @@ if __name__ == "__main__":
     print(theta_1)
     print(km[0])
     print(equasion(theta_0, theta_1, normalize_one(km[0], km)))
+    print(standard_error(km_n, price, theta_0, theta_1))
+
+    plt.scatter(km, price)
+    # plt.plot()
+    plt.plot([min(km), max(km)], [equasion(theta_0, theta_1, normalize_one(min(km), km)), equasion(theta_0, theta_1, normalize_one(max(km), km))])
+    plt.show()
